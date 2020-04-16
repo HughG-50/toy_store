88ab2f92862465c5c1dd6f36f555779002014dd3
hughgreethead <hugh.greethead@gmail.com>
2020-04-16 17:00:12 +1000

    Added seeding

---
+++ app/models/toy.rb
@@ -0,0 +1,4 @@
+class Toy < ApplicationRecord
+    # If I ever needed to break naming convention but still have the model hooked up to the database tables
+    # self.table_name = "toys"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 0..20 do
+    Toy.create(
+        name: Faker::DcComics.hero,
+        description: Faker::DcComics.title,
+        date_posted: Faker::Date.between(from: 1.year.ago, to: Date.today),
+        user: Faker::FunnyName.name
+    )
+    puts "Created toy: #{i}"
+end
