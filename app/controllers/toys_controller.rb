c366fce4e471eb1a81576c4ef6834505df46a26b
hughgreethead <hugh.greethead@gmail.com>
2020-04-17 13:28:41 +1000

    CRUD controller methods updated

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -21,17 +21,35 @@ class ToysController < ApplicationController
         user = params["user"]
         Toy.create(name: name, description: description, date_posted: date_posted, user: user)
         # is this necessary or the correct way to do it?
-        render json: 
+        # Just a return value for REST actions so we know something has happened?
+        new_toy = {"name" => name, "description" => description, "date_posted" => date_posted, "user" => user}
+        render json: new_toy
     end
 
     #Update a toy
     def update 
-
+        puts params["name"]
+        name = params["name"]
+        description = params["description"]
+        date_posted = params["date_posted"]
+        user = params["user"]
+        toy_to_update = Toy.find_by_name(name)
+        toy_to_update.description = description
+        toy_to_update.date_posted = date_posted
+        toy_to_update.user = user
+        toy_to_update.save
+        
+        render json: toy_to_update
     end
 
     #Remove a toy
     def destroy 
+        puts params["name"]
+        name = params["name"]
+        toy_to_delete = Toy.find_by_name(name)
+        toy_to_delete.destroy
 
+        render json: toy_to_delete
     end
 
     private 
